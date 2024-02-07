0824fc223104d732f2e1247735d9399dfb2ee77d
Macmini M2 <macminim2@Mac-mini-M2.local>
2024-02-07 23:43:34 +0700

    Setup project

---
+++ shopping_list/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+class HelloAPIView(APIView):
+    def get(self, request):
+        return Response({'message': 'Hello, API!'}, status=status.HTTP_200_OK)
